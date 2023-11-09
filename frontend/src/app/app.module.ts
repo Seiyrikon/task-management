0b8c95681e111c32c849eec74e66179931677e23
Ricky Galpo <rickygalpo7777@gmail.com>
2023-11-09 11:19:03 +0800

    sample templates on frontend

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { TopNavigationComponent } from './components/top-navigation/top-navigation.component';
+import { MainContentComponent } from './components/main-content/main-content.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopNavigationComponent,
+    MainContentComponent
   ],
   imports: [
     BrowserModule
