e79a3d442acb61c7f45498ff4e18f146730c369e
Ricky Galpo <rickygalpo7777@gmail.com>
2023-11-14 07:39:26 +0800

    modified folder structure of services

--- frontend/src/app/components/main-content/main-content.component.ts
+++ frontend/src/app/components/main-content/main-content.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { TaskService } from 'src/app/services/task.service';
+import { TaskService } from 'src/app/services/task/task.service';
 
 @Component({
   selector: 'main-content',
