3a49fbd7ac944d60725f14e3142e987a82b84c03
Ricky Galpo <rickygalpo7777@gmail.com>
2023-11-16 12:50:01 +0800

    created edit task form

---
+++ frontend/src/app/components/main-content/edit-task-form/edit-task-form.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-edit-task-form',
+  templateUrl: './edit-task-form.component.html',
+  styleUrls: ['./edit-task-form.component.css']
+})
+export class EditTaskFormComponent {
+
+}
